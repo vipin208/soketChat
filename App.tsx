063750e891f402661d2f05a126842cec2291d6bc
vipin jaiswal <114270762+vipin208@users.noreply.github.com>
2024-12-29 18:50:55 +0530

    initial command for frontend

--- App.tsx
+++ App.tsx
@@ -1,118 +1,126 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { FlatList, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client'
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState<any>([]);
+  const socketRef = useRef<any>(null);
+  useEffect(() => {
+    const socket = io('http://192.168.1.129:3000');
+    socketRef.current = socket;
+    socket.on('message', (msg) => {
+      if (msg.sender !== socket.id) { 
+        setChatMessages((prevMessages: any) => [msg, ...prevMessages]);
+      }
+    });
+    return () => {
+      socket.disconnect();
+    };
+  },[])
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const sendMessage = () => {
+    const socket = socketRef.current;
+    console.log('socket.idfront', socket.id)
+    if (message.trim() !== '') {
+      socket.emit('message', message);
+       setChatMessages((prevMessages:any) => [{  text: message,sender:socket.id  },...prevMessages]);
+      setMessage('');
+    }
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <FlatList
+         data={chatMessages}
+        keyExtractor={(item, index) => index.toString()}
+        inverted
+        contentContainerStyle={{top:'10%'}}
+        renderItem={({ item }) => (
+          <View style={[styles.messageContainer,{
+            alignSelf: item.sender === socketRef.current.id ? 'flex-end' : 'flex-start',
+          backgroundColor: item.sender === socketRef.current.id ? '#DCF8C6' : '#FFFFFF',
+            }]}>
+            <Text style={styles.username}>{item.sender.substring(0,3)}:</Text>
+            <Text style={styles.message}>{item.text}</Text>
+          </View>
+        )}
+        style={styles.chatArea}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type your message..."
+          value={message}
+          onChangeText={setMessage}
+        />
+        <Pressable style={styles.sendButton} onPress={sendMessage}>
+          <Text style={styles.sendButtonText}>Send</Text>
+        </Pressable>
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingHorizontal:20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  chatArea: {
+    flex: 1,
+    padding: 10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  messageContainer: {
+    flexDirection: 'row',
+    marginVertical: 5,
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.1,
+    shadowRadius: 1,
+    width: '50%',
   },
-  highlight: {
-    fontWeight: '700',
+  username: {
+    fontWeight: 'bold',
+    marginRight: 5,
   },
-});
-
-export default App;
+  message: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: '#ffffff',
+    borderTopWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 25,
+    bottom:40
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    backgroundColor: '#f9f9f9',
+  },
+  sendButton: {
+    marginLeft: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  sendButtonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+})
